4a5d7a8d2948ab47af60179995be21cc9b254d5a
duolong <tomato.stao@gmail.com>
2021-06-30 17:53:20 +0800

    feat: 增加Changelog

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,7 +1,15 @@
 declare class ClipboardCopy {
     private str;
     constructor(text: string);
+    /**
+     * 设置文案
+     * @param text 文案内容
+     * @returns this 当前实例
+     */
     setText(text: string): this;
+    /**
+     * 复制方法
+     */
     copy(): Promise<Error | void>;
     private copyClipboardApi;
     private copyExecCommand;
